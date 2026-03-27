caaeb761b94e99a5cbd8aeda8b6ed4ba627845ab
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-03-27 02:21:31 -0500

    refactor: restructure navbar into left center right layout using grid.

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,30 +1,45 @@
-import { Menu } from "lucide-react";
+import { Menu, Moon, Sun } from "lucide-react";
+import { useState } from "react";
 import SearchBar from "../molecules/SearchBar";
 import NotificationBell from "../molecules/NotificationBell";
 import UserMenu from "../molecules/UserMenu";
 
 export default function Navbar({ toggleSidebar }) {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <header className="flex items-center justify-between px-4 bg-white shadow h-17.5">
+    <header className="grid grid-cols-[auto_1fr_auto] items-center px-4 bg-white shadow h-17.5">
       
       {/* LEFT */}
-      <div className="flex items-center gap-4 w-full">
-        
-        {/* Hamburger */}
+      <div className="flex items-center">
         <button
           onClick={toggleSidebar}
-          className="p-2 rounded-lg cursor-pointer hover:bg-gray-100 transition"
+          className="p-2 rounded-lg hover:bg-gray-100 transition"
         >
           <Menu className="w-6 h-6 text-gray-700" />
         </button>
+      </div>
 
-        {/* Search */}
+      {/* CENTER */}
+      <div className="flex justify-center">
         <SearchBar />
       </div>
 
       {/* RIGHT */}
-      <div className="flex items-center gap-3 ml-4">
+      <div className="flex items-center justify-end gap-3">
         <NotificationBell />
+
+        <button
+          onClick={() => setDarkMode(!darkMode)}
+          className="p-2 rounded-lg hover:bg-gray-100 transition"
+        >
+          {darkMode ? (
+            <Sun className="w-5 h-5 text-gray-700" />
+          ) : (
+            <Moon className="w-5 h-5 text-gray-700" />
+          )}
+        </button>
+
         <UserMenu />
       </div>
     </header>
