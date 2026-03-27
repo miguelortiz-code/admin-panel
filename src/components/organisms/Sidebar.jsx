6c434c60ddb7b2808f3d79067dce868ddec46aca
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-03-27 02:05:58 -0500

    feat: upgrade sidebar with animated tooltips and smooth submenu transitions.

--- src/components/organisms/Sidebar.jsx
+++ src/components/organisms/Sidebar.jsx
@@ -88,7 +88,7 @@ export default function Sidebar({ isCollapsed }) {
                         if (item.submenu) toggleMenu(item.name);
                       }}
                       className={`
-                        relative group flex items-center justify-between p-2 rounded-lg cursor-pointer transition
+                        relative group flex items-center justify-between p-2 rounded-lg cursor-pointer transition-all duration-200
                         ${isActive ? "bg-gray-800" : "hover:bg-gray-800"}
                       `}
                     >
@@ -104,7 +104,7 @@ export default function Sidebar({ isCollapsed }) {
                       {/* Arrow */}
                       {!isCollapsed && item.submenu && (
                         <ChevronDown
-                          className={`w-4 h-4 transition-transform ${
+                          className={`w-4 h-4 transition-transform duration-200 ${
                             isOpen ? "rotate-180" : ""
                           }`}
                         />
@@ -112,27 +112,45 @@ export default function Sidebar({ isCollapsed }) {
 
                       {/* TOOLTIP */}
                       {isCollapsed && (
-                        <span className="
-                          absolute left-14 bg-black text-white text-xs px-2 py-1 rounded
-                          opacity-0 group-hover:opacity-100 transition
-                          whitespace-nowrap
-                        ">
+                        <span
+                          className="
+                            absolute left-full ml-3
+                            bg-gray-800 text-white text-xs px-2 py-1 rounded-md
+                            opacity-0 translate-x-2
+                            group-hover:opacity-100 group-hover:translate-x-0
+                            transition-all duration-200 ease-in-out
+                            whitespace-nowrap
+                            z-50
+                            pointer-events-none
+                          "
+                        >
                           {item.name}
                         </span>
                       )}
                     </div>
 
                     {/* SUBMENU */}
-                    {!isCollapsed && isOpen && item.submenu && (
-                      <div className="ml-8 mt-1 flex flex-col gap-1 text-sm text-gray-300">
-                        {item.submenu.map((sub) => (
-                          <span
-                            key={sub}
-                            className="hover:text-white cursor-pointer"
-                          >
-                            {sub}
-                          </span>
-                        ))}
+                    {!isCollapsed && item.submenu && (
+                      <div
+                        className={`
+                          overflow-hidden transition-all duration-300 ease-in-out
+                          ${
+                            isOpen
+                              ? "max-h-40 opacity-100 mt-1"
+                              : "max-h-0 opacity-0"
+                          }
+                        `}
+                      >
+                        <div className="ml-8 flex flex-col gap-1 text-sm text-gray-300">
+                          {item.submenu.map((sub) => (
+                            <span
+                              key={sub}
+                              className="hover:text-white cursor-pointer"
+                            >
+                              {sub}
+                            </span>
+                          ))}
+                        </div>
                       </div>
                     )}
                   </div>
